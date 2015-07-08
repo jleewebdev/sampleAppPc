3884128162154b12a3f2205a802c7daf85743a5c
Josh Lee <lee45276@gmail.com>
2015-07-08 08:06:36 +0800

    create_group migration and created Group model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707235403) do
+ActiveRecord::Schema.define(version: 20150708000355) do
+
+  create_table "groups", force: true do |t|
+    t.string   "user"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
