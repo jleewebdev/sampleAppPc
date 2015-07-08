a0c21ae3482195e0ec94bb25f89a5236cf094ddc
Josh Lee <lee45276@gmail.com>
2015-07-08 09:34:32 +0800

    Created index page for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'groups#index'
-  resources :users
+  resources :users, :posts, :groups
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
