03504927fc710d543d8292a72e8ee11d41ec7586
Josh Lee <lee45276@gmail.com>
2015-07-08 20:17:59 +0800

    Added timestamps to post table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708120211) do
+ActiveRecord::Schema.define(version: 20150708121448) do
 
   create_table "groups", force: true do |t|
     t.string   "user"
@@ -21,10 +21,12 @@ ActiveRecord::Schema.define(version: 20150708120211) do
   end
 
   create_table "posts", force: true do |t|
-    t.string  "title"
-    t.string  "url"
-    t.integer "user_id"
-    t.integer "description"
+    t.string   "title"
+    t.string   "url"
+    t.integer  "user_id"
+    t.integer  "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "user_groups", force: true do |t|
