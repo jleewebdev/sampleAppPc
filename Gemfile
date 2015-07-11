5cae16e0f962e9056c4fe222bb6bbc4aa2a99f88
Josh Lee <Lee45276@gmail.com>
2015-07-11 21:35:43 +0800

    Can create posts from show groups view, but post is not  linked to the group. Need to find a way how to link the two together

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'http://rubygems.org'
 
+require 'Pry'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,19 @@ class PostsController < ActionController::Base
     @post = Post.new
   end
 
+  def create 
+    @post = Post.new
+    if @post.save
+      redirect_to groups_path
+    else
+      render "groups/index"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit!
+  end
+
 end
